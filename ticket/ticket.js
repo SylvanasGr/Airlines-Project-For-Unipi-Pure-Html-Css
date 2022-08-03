2ead901ba15a87c9fafceb96780718d368791615
Konstantinos Kolios <konstantinoskolios@windowslive.com>
2022-08-04 01:03:52 +0300

    okk

--- ticket/ticket.js
+++ ticket/ticket.js
@@ -16,8 +16,11 @@ const state = document.getElementById("state");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 const postal = document.getElementById("postal");
-const dfrom = document.getElementById("dfrom").value;
-const dto = document.getElementById("dto").value;
+const dfrom = document.getElementById("dfrom");
+const dto = document.getElementById("dto");
+const card = document.getElementById("card");
+const cld = document.getElementById("cld");
+const cardxexpdate = document.getElementById("cardxexpdate");
 let sending = [];
 
 form.addEventListener("submit", (e) => {
@@ -50,54 +53,89 @@ const setSuccess = (element) => {
   errorDisplay.innerText = "";
   inputControl.classList.add("success");
   inputControl.classList.remove("error");
-  if (sending.length > 0) {
-    sending.pop();
-  }
+
 };
 
-
-const latinMsg = "This field must contains only latin characters and it's required.";
+const latinMsg =
+  "This field must contains only latin characters and it's required.";
 
 const validateInputs = (e) => {
   const firstnameValue = firstname.value.trim();
   const lastnameValue = lastname.value.trim();
   const emailValue = email.value.trim();
   const postalValue = postal.value.trim();
-  // const phone = phone.value;
+  const phoneValue = phone.value.trim();
+  const cldValue = cld.value.trim();
+  const cardValue = card.value.trim();
+  const cardxexpdateValue = cardxexpdate.value.trim()
 
-  if (!/^[a-zA-Z]+$/.test(firstnameValue) || firstnameValue == '') {
+  if ( firstnameValue == "" || !/^[a-zA-Z]+$/.test(firstnameValue) ) {
     setError(firstname, latinMsg);
   } else {
     setSuccess(firstname);
   }
 
-  if (!/^[a-zA-Z]+$/.test(lastnameValue) || lastnameValue == '') {
+  if (firstnameValue == "" || !/^[a-zA-Z]+$/.test(lastnameValue) || lastnameValue == "") {
     setError(lastname, latinMsg);
   } else {
     setSuccess(lastname);
   }
 
-  if(!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailValue) || emailValue == ''){
-    setError(email,"Email must contains something like this :  example@whatever.com");
-  }else{
+  if (
+    emailValue == "" ||
+    !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      emailValue) 
+  ) {
+    setError(
+      email,
+      "Email must contains something like this :  example@whatever.com"
+    );
+  } else {
     setSuccess(email);
   }
 
-  alert("posta value : " + postalValue);
-  if(postalValue == '' || !/^\d+$/.test(postalValue)){
+  if (phoneValue == "" || !/^\d+$/.test(phoneValue)) {
+    setError(phone, "Postal field must contains 10 digits without letters.");
+  } else {
+    setSuccess(phone);
+  }
+
+  if (postalValue == "" || !/^\d+$/.test(postalValue)) {
     setError(postal, "Postal field must contains 5 digits without letters.");
-  }else{
+  } else {
     setSuccess(postal);
   }
 
-  alert("df : " + dfrom + "dt : " + dto);
-  if(dfrom == dto){
-    alert("you cant.");
+  
+  if (cldValue == "" || !/^\d+$/.test(cldValue)) {
+    setError(cld, "Card Last 3 Digits  field must contains 3 digits without letters.");
   }else{
-    alert("goood!")
+    setSuccess(cld);
   }
 
-  alert("total erros : " + sending.length)
+  if (dfrom.value == dto.value) {
+        setError(
+      dto,
+      "Cant fly for the same destination:" +
+        "example : Crete - Crete is not acceptable." +
+        "example : Crete - Naxos is accepatable");
+  } else {
+    setSuccess(dto);
+  }
+
+  if (cardValue == "" || !/^\d+$/.test(cardValue)) {
+    setError(card, "Card field must contains 16 digits without letters.");
+  } else {
+    setSuccess(card);
+  }
+
+
+  if(cardxexpdateValue ==""){
+    setError(cardxexpdate,"Please put a valid date");
+  }else{
+    setSuccess(cardxexpdate);
+  }
+
   if (sending.length === 0) {
     var myFormAction = document.getElementById("myForm") || null;
     if (myFormAction) {
--- user/user.js
+++ user/user.js
@@ -1,51 +1,243 @@
-// // todo: change the path to the corresponding urls.
+document.getElementById("loginForm").style.display='none';
+document.getElementById("createForm").style.display='none'
 
-// var myFormAction = document.getElementById("myForm") || null;
-// if (myFormAction) {
-//   myFormAction.action =
-//     "http://localhost/php_labs/Airlines-Project-For-Unipi-Pure-Html-Css/ticket/ticket.php";
+function showLogin(){
+  document.getElementById("loginForm").style.display='block';
+  document.getElementById("createForm").style.display='none'
+}
+
+function showCreate(){
+  document.getElementById("loginForm").style.display='none';
+  document.getElementById("createForm").style.display='block'
+}
+
+
+// // // todo: change the path to the corresponding urls.
+
+// var searchAction = document.getElementById("search") || null;
+// if (searchAction) {
+//   searchAction.action =
+//     "http://localhost/php_labs/Airlines-Project-For-Unipi-Pure-Html-Css/ticket/search.php";
 // }
 
-// pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+// const latinValidation = !/^[a-zA-Z]+$/;
+const formLogin = document.getElementById("loginForm");
+const lemail = document.getElementById("lemail");
+const lpassword = document.getElementById("lpassword");
+// const form = document.getElementById("createForm");
+const errorElement = document.getElementById("error");
+// const firstname = document.getElementById("firstname");
+// const lastname = document.getElementById("lastname");
+// const birthdate = document.getElementById("birthdate");
+// const state = document.getElementById("state");
+// const phone = document.getElementById("phone");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const repeatpassword = document.getElementById("repeatpassword");
+// const postal = document.getElementById("postal");
+// const dfrom = document.getElementById("dfrom");
+// const dto = document.getElementById("dto");
+// const card = document.getElementById("card");
+// const cld = document.getElementById("cld");
+// const cardxexpdate = document.getElementById("cardxexpdate");
+let sending = [];
+formLogin.addEventListener("submit", (e) => {
+  validateInputsLogin(e);
+})
 
-function showError(errorElement, errorMessage) {
-  document.querySelector("." + errorElement).classList.add("display-error");
-  document.querySelector("." + errorElement).innerHTML = errorMessage;
-}
-
-function onSubmit() {
-  var firstname = document.getElementById("firstname").value;
-  var lastname = document.getElementById("lastname").value;
-  var state = document.getElementById("state").value;
-  var password = document.getElementById("password").value;
-  // var birthdate = document.getElementById('birthdate').value;
-  // var phone = document.getElementById('phone').value;
-  // var email = document.getElementById('email').value;
-  // var dt = document.getElementById('df').value;
-  // var df = document.getElementById('dt').value;
-  // var timeflight = document.getElementById('timeflight').value;
-  // var cardtype = document.getElementById('cardtype').value;
-  // var card = document.getElementById('card').value;
-  // var carxexpdate = document.getElementById('carxexpdate').value;
-  // var cardlastdigits = document.getElementById('cardlastdigits').value;
-  if (!/^[a-zA-Z]+$/.test(firstname)) {
-    showError("firstname-error", "Only latin characters accepted.");
-  }
-  if (!/^[a-zA-Z]+$/.test(lastname)) {
-    showError("lastname-error", "Only latin characters accepted.");
-  }
-  if (!/^[a-zA-Z]+$/.test(state)) {
-    showError("state-error", "Only latin characters accepted.");
-  }
-  if(!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password)){
-    showError("password-error" ,"The password must contains at least one lowercase,uppercase and one number too.")
-  }
-}
+form.addEventListener("submit", (e) => {
+  validateInputsCreate(e);
+});
 
 function onReset() {
-  document.getElementById("myForm2").reset();
+  document.getElementById("createForm").reset();
+  errorElement.innerText = null;
 }
 
 function onPrint() {
   window.print();
 }
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+  sending.push("error");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+// const latinMsg =
+//   "This field must contains only latin characters and it's required.";
+
+const validateInputsCreate = (e) => {
+  // const firstnameValue = firstname.value.trim();
+  // const lastnameValue = lastname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const repeatpasswordValue = repeatpassword.value.trim();
+  // const postalValue = postal.value.trim();
+  // const phoneValue = phone.value.trim();
+  // const cldValue = cld.value.trim();
+  // const cardValue = card.value.trim();
+  // const cardxexpdateValue = cardxexpdate.value.trim()
+
+  // if ( firstnameValue == "" || !/^[a-zA-Z]+$/.test(firstnameValue) ) {
+  //   setError(firstname, latinMsg);
+  // } else {
+  //   setSuccess(firstname);
+  // }
+
+  // if (firstnameValue == "" || !/^[a-zA-Z]+$/.test(lastnameValue) || lastnameValue == "") {
+  //   setError(lastname, latinMsg);
+  // } else {
+  //   setSuccess(lastname);
+  // }
+
+  // if (
+  //   emailValue == "" ||
+  //   !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+  //     emailValue
+  //   )
+  // ) {
+  //   setError(
+  //     email,
+  //     "Email must contains something like this :  example@whatever.com"
+  //   );
+  // } else {
+  //   setSuccess(email);
+  // }
+
+  // if (passwordValue == "" || !/^\d+$/.test(passwordValue)) {
+  //   setError(
+  //     password,
+  //     "Password must be not be null and must contains one lowercase,one uppercase and one digit."
+  //   );
+  // } else {
+  //   setSuccess(password);
+  // }
+
+  // if (repeatpasswordValue == "" || !/^\d+$/.test(repeatpasswordValue)) {
+  //   setError(
+  //     repeatpassword,
+  //     "Password must be not be null and must contains one lowercase,one uppercase and one digit."
+  //   );
+  // } else {
+  //   setSuccess(repeatpassword);
+  // }
+
+
+  // if(passwordValue !== repeatpasswordValue){
+  //   setError(repeatpassword,"Not same passwords");
+  // }else{
+  //   setSuccess(repeatpassword);
+  // }
+
+  // if (passwordValue === repeatpasswordValue && passwordValue !== "") {
+  //   alert("true");
+  // } else {
+  //   alert("false");
+  // }
+
+  // if (phoneValue == "" || !/^\d+$/.test(phoneValue)) {
+  //   setError(phone, "Postal field must contains 10 digits without letters.");
+  // } else {
+  //   setSuccess(phone);
+  // }
+
+  // if (postalValue == "" || !/^\d+$/.test(postalValue)) {
+  //   setError(postal, "Postal field must contains 5 digits without letters.");
+  // } else {
+  //   setSuccess(postal);
+  // }
+
+  // if (cldValue == "" || !/^\d+$/.test(cldValue)) {
+  //   setError(cld, "Card Last 3 Digits  field must contains 3 digits without letters.");
+  // }else{
+  //   setSuccess(cld);
+  // }
+
+  // if (dfrom.value == dto.value) {
+  //       setError(
+  //     dto,
+  //     "Cant fly for the same destination:" +
+  //       "example : Crete - Crete is not acceptable." +
+  //       "example : Crete - Naxos is accepatable");
+  // } else {
+  //   setSuccess(dto);
+  // }
+
+  // if (cardValue == "" || !/^\d+$/.test(cardValue)) {
+  //   setError(card, "Card field must contains 16 digits without letters.");
+  // } else {
+  //   setSuccess(card);
+  // }
+
+  // if(cardxexpdateValue ==""){
+  //   setError(cardxexpdate,"Please put a valid date");
+  // }else{
+  //   setSuccess(cardxexpdate);
+  // }
+  alert(sending.length);
+  if (sending.length === 0) {
+    var myFormAction = document.getElementById("createForm") || null;
+    if (myFormAction) {
+      myFormAction.action =
+        "http://localhost/php_labs/Airlines-Project-For-Unipi-Pure-Html-Css/ticket/ticket.php";
+    }
+  } else {
+    e.preventDefault();
+    sending = [];
+  }
+};
+
+const validateInputsLogin = (e) => {
+
+  const lemailValue = lemail.value.trim();
+  const lpasswordValue = lpassword.value.trim();
+
+  if (
+    lemailValue == "" ||
+    !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      lemailValue
+    )
+  ) {
+    setError(
+      lemail,
+      "Email must contains something like this :  example@whatever.com"
+    );
+  } else {
+    setSuccess(lemail);
+  }
+
+  if (lpasswordValue == "" || !/^\d+$/.test(lpasswordValue)) {
+    setError(
+      lpassword,
+      "Password must be not be null and must contains one lowercase,one uppercase and one digit."
+    );
+  } else {
+    setSuccess(lpassword);
+  }
+
+  alert(sending.length);
+  if (sending.length === 0) {
+    var myFormAction = document.getElementById("createForm") || null;
+    if (myFormAction) {
+      alert("wraios re man");
+      e.preventDefault();
+  } else {
+    e.preventDefault();
+    sending = [];
+  }
+}
+};
